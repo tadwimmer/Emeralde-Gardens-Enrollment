0b0751fbd2d490519350da629c9bf575582734bb
Tad Wimmer <tad.wimmer@utah.edu>
2020-01-23 14:10:51 -0700

    Initial Commit

---
+++ EmeraldeGardensEnrollment/src/main/java/com/imhotek/EmeraldeGardensEnrollment/EmeraldeGardensEnrollmentApplication.java
@@ -0,0 +1,13 @@
+package com.imhotek.EmeraldeGardensEnrollment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmeraldeGardensEnrollmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmeraldeGardensEnrollmentApplication.class, args);
+	}
+
+}
---
+++ EmeraldeGardensEnrollment/src/test/java/com/imhotek/EmeraldeGardensEnrollment/EmeraldeGardensEnrollmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imhotek.EmeraldeGardensEnrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmeraldeGardensEnrollmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
