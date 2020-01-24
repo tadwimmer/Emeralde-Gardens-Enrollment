0da0de00111194ae39e469c80e1213f24c50fd3f
Tad Wimmer <tad.wimmer@utah.edu>
2020-01-23 17:05:52 -0700

    Added Client interface

---
+++ EmeraldeGardensEnrollment/src/main/java/com/imhotek/EmeraldeGardensEnrollment/model/Client.java
@@ -0,0 +1,16 @@
+package com.imhotek.EmeraldeGardensEnrollment.model;
+
+import java.time.LocalDate;
+
+public interface Client {
+
+    String getFirstName();
+    String getLastName();
+    LocalDate getBirthDate();
+
+
+
+
+
+
+}
